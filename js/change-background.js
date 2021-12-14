27d0bda468351765067e45716654d9b296215183
M <m@streamline.digital>
2021-12-14 12:26:00 +0700

    Adding files for the basic site example

---
+++ js/change-background.js
@@ -0,0 +1,11 @@
+function changeBackground() {
+
+  document.getElementById("root").style.backgroundImage = 'url("img/bg.jpg")'
+
+  document.getElementById("bg-button").style.visibility = 'hidden';
+
+}
+
+
+
+
